3712f06725744ab1f865da30f71d7c762ac8b9dd
analaura3s <analauras.ifc.info3@gmail.com>
2026-08-12 13:42:07 -0300

    Feat: unidade 5

--- vite.config.js
+++ vite.config.js
@@ -44,7 +44,7 @@ export default defineConfig({
             },
           },
           {
-            urlPattern: /^https:\/\/api\.exemplo\.com\/.*/i,
+            urlPattern: /^http:\/\/localhost:8001\/.*/i,
             handler: 'NetworkFirst',
             options: {
               cacheName: 'api-cache',
@@ -52,10 +52,10 @@ export default defineConfig({
                 maxEntries: 50,
                 maxAgeSeconds: 60 * 60 * 24, // 24 horas
               },
+              networkTimeoutSeconds: 10,
               cacheableResponse: {
                 statuses: [0, 200],
               },
-              networkTimeoutSeconds: 10,
             },
           },
         ],
